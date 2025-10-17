6f6d8a0ef1ce2447bbf9038f7770c7d98443dc54
SirIyke0147 <okorieikennakalu@gmail.com>
2025-10-17 21:17:58 +0100

    added a 3rd file

--- Demo.py
+++ Demo.py
@@ -1,3 +1,4 @@
 print ("recent changes")
 
 #this is my second demo file for git and github.
+print ("merge conflict resolved with pull rebased")
---
+++ demo2.py
@@ -0,0 +1,2 @@
+#this is my last demo file for git and github.
+print ("This is Demo file for git and github")
